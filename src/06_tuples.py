57a2e3ae897324d4ae23eca23bc667d54e5ac898
Krishan Nattar <krishan.nattar@gmail.com>
2019-08-12 23:00:40 -0700

    Tuples complete

--- src/06_tuples.py
+++ src/06_tuples.py
@@ -36,6 +36,12 @@ print("Distance is: {:.2f}".format(dist(a, b)))
 
 # YOUR CODE HERE
 
+def print_tuple(tup):
+    if isinstance(tup, tuple) != True:
+        tup=tuple([tup])
+    for num in tup:
+        print(num)
+
 t = (1, 2, 5, 7, 99)
 print_tuple(t)  # Prints 1 2 5 7 99, one per line
 
